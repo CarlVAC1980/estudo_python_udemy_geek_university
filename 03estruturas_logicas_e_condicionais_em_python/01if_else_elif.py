e055e182fb955e65b3b04f552345b500eca6d07b
Carlos M Valenca <cavac10@gmail.com>
2022-09-05 07:25:48 -0300

    Estruturas condicionais if, else, elif

---
+++ 03estruturas_logicas_e_condicionais_em_python/01if_else_elif.py
@@ -0,0 +1,36 @@
+"""
+Estruturas condicionais
+if, else, elif
+"""
+
+idade = 18
+
+"""
+# Estrutura condicional if, else em C
+
+if(idade < 18){
+    printf("Menor de idade");
+}else if(idade == 18){
+    printf("Tem 18 anos");
+}else{
+    printf("Maior de idade");
+}
+"""
+"""
+# Estrutura condicional if, else em Java
+
+if(idade < 18){
+    System.out.printn("Menor de idade");
+}else if(idade == 18){
+    System.out.printn("Tem 18 anos");
+}else{
+    System.out.printn("Maior de idade");
+}
+"""
+
+if idade < 18:
+    print('Menor de idade')
+elif idade == 18:
+    print('Tem 18 anos')
+else:
+    print('Maior de idade')
