e25962e4d28a5c33041bccce8102f2e3a5be9d96
Carlos M Valenca <cavac10@gmail.com>
2022-09-05 08:21:55 -0300

    Exercicio ficha 1 - exercicio2 11 - 20

--- 02variaveis_e_tipos_de_dados_em_python/exercicios/exercicio2.py
+++ 02variaveis_e_tipos_de_dados_em_python/exercicios/exercicio2.py
@@ -50,3 +50,54 @@ pi = 3.14
 g = r * 180 / pi
 
 print(f'O valor do ângulo em graus é: {g}')
+
+"""
+16 Leia um valor de comprimento em polegadas e apresente-o convertido em centimetros.
+A fórmula de conversão é: C = P * 2.54, sendo C o comprimento em centimetros e P o 
+copmpirmento em polegadas.
+"""
+p = float(input('Digite um valor em polegadas: '))
+
+c = p * 2.54
+
+print(f'O valor em centimetros é: {c}')
+"""
+17 Leia um valor de comprimento em centimentros e apresente-o convertido em polegadas.
+A fórmula de conversão é: P = C/2.54 
+"""
+c = float(input('Digite um valor em centimentros: '))
+
+p = c/2.54
+
+print(f'o valor em polegadas é: {p}')
+
+"""
+18 Leia um valor de volume em metros cubicos m³ e apresente-o convertido em Litros.
+A fórmula de conversão é: L = 1000 * M, sendo L o volume em litros e M o volume em
+metros cúbicos
+"""
+m = float(input('Digite um valor em metros cúbicos: '))
+
+l = 1000 * m
+
+print(f'O valor em litros é: {l}')
+"""
+19 Leia um valor de volume em Litros e apresente-o convertido em metros cúbicos.
+A fórmula de conversão é: M = L/1000, sendo L o volume em litros e M o volume em
+metros cúbicos
+"""
+l = float(input('Digite um valor em litros: '))
+
+m = l/1000
+
+print(f'O valor em metros cúbicos é: {m}')
+
+"""
+20 Leia um valor de massa em quilogramas e apresente-o em libras. A fórmula 
+de conversão é: L = K/0.45, sendo K a massa em quilogramas e l a massa em libras
+"""
+k = float(input('Digite o valor em quilogramas: '))
+
+l = k / 0.45
+
+print(f'O valor em libras é: {l}')
