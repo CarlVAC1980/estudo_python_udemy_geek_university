c73e7eb8cbb955a85d70f32b08012de640406c84
Carlos M Valenca <cavac10@gmail.com>
2022-09-06 08:42:33 -0300

    Estrutura de repetição, loop for

---
+++ 04estruturas_de_repeticao_em_python/01loop_for.py
@@ -0,0 +1,21 @@
+"""
+Estrutura de repetição Loop for
+
+Loop  -> Estrutura de repetição
+For -> Uma dessas estruturas
+
+C ou Java
+
+for(int i = ; i < 10; i++){
+    //execução do loop
+}
+
+Python
+
+for item in interavel:
+    //execução do loop
+
+
+
+Utilizamos loops para iterar sobre sequencias ou sobre valores iteraveis
+"""
