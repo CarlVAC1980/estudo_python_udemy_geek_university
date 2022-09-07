368610a9df7d6509304e9b807d1d46e95c15a936
Carlos M Valenca <cavac10@gmail.com>
2022-09-07 06:39:18 -0300

    Estrutura de repetição, loop for 22:23

--- 04estruturas_de_repeticao_em_python/01loop_for.py
+++ 04estruturas_de_repeticao_em_python/01loop_for.py
@@ -15,8 +15,6 @@ Python
 for item in interavel:
     //execução do loop
 
-
-
 Utilizamos loops para iterar sobre sequencias ou sobre valores iteraveis
 
 Exemplos de iteráveis:
@@ -26,10 +24,6 @@ Exemplos de iteráveis:
     lista = [1, 3, 5, 7, 9]
 - Range
     numeros = range(1, 10)
-"""
-nome = 'Geek University'
-lista = [1, 3, 5, 7, 9]
-numeros = range(1, 10) # Temos de transformar em lista
 
 #Exemplo de for 1 (Iterando em uma string)
 for letra in nome:
@@ -40,10 +34,24 @@ for numero in lista:
     print(numero)
 
 # Exemplo de for 3 (Iterando sobre range)
-"""
+
 range(valor_inicial, valor_final)
 
-OBS: O valor final não incluso 
-"""
+OBS: O valor final não incluso
+
 for numero in range(1, 10):
     print(numero)
+
+Enumerate:
+
+((0, 'G'), (1, 'e'), (2, 'e'), (3, 'k'), (4, ' '), (5, 'U')...)
+
+for indice, letra in enumerate(nome):
+    print(nome[indice])
+
+"""
+nome = 'Geek University'
+lista = [1, 3, 5, 7, 9]
+numeros = range(1, 10) # Temos de transformar em lista
+
+
