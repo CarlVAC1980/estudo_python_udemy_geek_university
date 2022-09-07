baf08878dc34921fca9406062ddca065da5ca66a
Carlos M Valenca <cavac10@gmail.com>
2022-09-07 08:22:58 -0300

    Exercicio ficha 1 - exercico de 31 a 40

--- 02variaveis_e_tipos_de_dados_em_python/exercicios/exercicio4.py
+++ 02variaveis_e_tipos_de_dados_em_python/exercicios/exercicio4.py
@@ -17,3 +17,35 @@ dobro = num * 2
 soma = (triplo + 1) + (dobro - 1)
 
 print(f'A soma do sucessor de {triplo} com o antecessor de {dobro} é {soma}')
+
+"""
+33 Leia o tamanho de um quadrado e imprima como resultado a sua área
+"""
+lado = float(input('Digite um doa lados de um quadrado: '))
+
+area = lado ** 2
+
+print(f'A área do quadrado é {area}')
+
+"""
+34 Leia o valor do raio de um cpírculo e calcule e imprima a área do circulo correspondente.
+A área do cículo é pi * raio², considere pi = 3.141592
+"""
+raio = float(input('Digite o valor do raio do circulo: '))
+
+pi = 3.141592
+area = pi * (raio ** 2)
+
+print(f'A área do círculo é {area}')
+
+"""
+35 Seja a e b os catetos de um triângulo, onde a hipotenusa é obtida pela equação:
+hipotenusa = raiz(a² + b³). Faça um programa que receba os valores de a e b e calcule
+o valor da hipotenusa através da equação. imprima o resultado dessa operação.
+"""
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+
+hipotenusa = (a ** 2 + b ** 2) ** (1/2)
+
+print(f'O valor da hipotenusa é {hipotenusa}')
