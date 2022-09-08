54be2f443ec34c8f93c94eb8b9c01c4eb844a298
Carlos M Valenca <cavac10@gmail.com>
2022-09-08 07:53:15 -0300

    Exercicio folha 2 / exercicio 01 a 10

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio1.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio1.py
@@ -22,3 +22,26 @@ if num > 0:
     print(f'A Raiz do número é: {num ** (0.5)}')
 else:
     print('Este número é inválido.')
+
+"""
+3 Leia um número real. Se o número for positivo imprima a rais quadrada. Do contrátio
+imprima o número ao quadrado.
+"""
+num = float(input('Digite um número: '))
+
+if num > 0:
+    print(f'A Raiz do número é: {num ** (0.5)}')
+else:
+    print(f'O quadrado do número é {num ** 2}')
+
+"""
+4 Faça um programa que leia um número e, caso ele seja positivo, calcule e mostre:
+- O número digitado ao quadrado
+- A rais quadrada do número digitado
+"""
+num = float(input('Digite um número: '))
+
+if num > 0:
+    print(f'O quadrado do número é {num ** 2}, '
+          f'\nA rais quadrada do número é {num **(0.5)}')
+
