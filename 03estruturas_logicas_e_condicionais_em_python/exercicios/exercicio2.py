5c151f44784a8eb7895f9848437888e789e906a6
Carlos M Valenca <cavac10@gmail.com>
2022-09-09 06:53:57 -0300

    Exercicio folha 2 / exercicio 11 a 20

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio2.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio2.py
@@ -6,8 +6,16 @@ a mensagem "Número inválido".
 """
 x = int(input('Digite um número inteiro maior que zero: '))
 
-
 if x < 0:
     print('Número inválido')
 else:
-    pass
+    soma = 0
+    y = str(x)
+    for i in range(len(y)):
+        soma += int(y[i])
+    print(soma)
+
+"""
+12 Ler um número inteiro. Se o número lido for negativo, escreva a mensagem 'Número 
+inválido'. Se o número for positivo, calcular o logaritmo deste número.
+"""
