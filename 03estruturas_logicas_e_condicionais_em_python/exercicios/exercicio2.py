8e544ff983723fa5b3d8ee36f6ec1c8b761b79b2
Carlos M Valenca <cavac10@gmail.com>
2022-09-12 08:40:46 -0300

    Exercicio 2 - folha 2 11 a 20

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio2.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio2.py
@@ -69,3 +69,80 @@ elif 3 <= media <= 4.9:
 else:
     print(f'A média do aluno é {media}, aluno APROVADO')
 
+"""
+15 Usando o switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia 
+da semana correspondente a este número, iste é, domingo se 1, segunda se 2, e
+assim por diante.
+"""
+dia = int(input('Digite o número referente ao dia: '))
+
+match dia:
+    case 1:
+        print('Domingo')
+    case 2:
+        print('Segunda-Feira')
+    case 3:
+        print('Terça-Feira')
+    case 4:
+        print('Quarta-Feira')
+    case 5:
+        print('Quinta-Feira')
+    case 6:
+        print('Sexta-Feira')
+    case 7:
+        print('Sábado')
+    case _:
+        print('Valor inválido')
+
+"""
+16 Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e imprima o mês
+correspondente a este número. Isto é, janeiro se 1, fevereiro se 2, e assim por diante. 
+"""
+mes = int(input('Digite o número referente ao mês: '))
+
+match mes:
+    case 1:
+        print('janeiro')
+    case 2:
+        print('fevereiro')
+    case 3:
+        print('março')
+    case 4:
+        print('abril')
+    case 5:
+        print('maio')
+    case 6:
+        print('junho')
+    case 7:
+        print('julho')
+    case 8:
+        print('agosto')
+    case 9:
+        print('setembro')
+    case 10:
+        print('outubro')
+    case 11:
+        print('novembro')
+    case 12:
+        print('dezembro')
+    case _:
+        print('Valor inválido')
+
+"""
+17 Faça um programa que calcule e mostre a área de um trapazeo. Sabe-se que:
+a = ((basemaior + basemenor) * altura) / 2
+Lembre-se que a base maior e a base menor devem ser números maiores que zero.
+"""
+bmr = float(input('Digite o valor da base maior: '))
+bmn = float(input('Digite o valor da base menor: '))
+alt = float(input('Digite o valor da altura: '))
+
+if bmr < 0 or bmn < 0:
+    print('Valor de uma das bases esta inválido')
+elif bmr < bmn:
+    print('Valor da base maior é menor que o valor da base menor, revise!')
+else:
+    a = ((bmr + bmn) * alt) / 2
+    print(f'A area do trapezeo é {a}')
+
+
