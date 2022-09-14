88e13fb49e944c0455769b83c5f9612932e531b2
Carlos M Valenca <cavac10@gmail.com>
2022-09-14 08:33:04 -0300

    Exercicio ficha 2 exercicios 21 a 30

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio3.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio3.py
@@ -115,4 +115,162 @@ else:
         raiz2 = (- b - math.sqrt(delta)) / (2 * a)
         print(f'As raizes são {raiz1} e {raiz2}')
 
+"""
+26 Leia a distância em Km e a quantidade de litros de gasolina consumidos por um carro 
+em um percurso, calcule o consumo em Km/l e escreva uma mensagem de acordo com
+a tabela abaixo:
+CONSUMO  -  (Km/l) - MENSAGEM
+menor que -  8    - Venda o carro!
+entre     - 8 e 14 - Econômico!
+maior que -  14   - Super econômico!
+"""
+km = float(input('Digite a distancia em quilometros: '))
+l = float(input('Digite a quantidade de litros: '))
 
+consumo = km / l
+
+if consumo < 8:
+    print('Venda o carro!')
+elif 8 <= consumo <= 14:
+    print('Econômico!')
+else:
+    print('Super econômico!')
+
+"""
+27 Escreva um programa que, dada a idade de um nadador, classifique-o em uma das 
+seguintes categorias:
+Categoria  -- idade
+Infantil A  - 5 a 7
+Infantil B  - 8 a 10
+Juvenil A  -  11 a 13
+Juvenil B  -  14 a 17
+Sênior    -   maiores de 18 anos
+"""
+idade = int(input('Digite a idade do nadador: '))
+
+if 5 <= idade <= 7:
+    print('Categoria : Infantil A')
+elif 8 <= idade <= 10:
+    print('Categoria: Infatil B')
+elif 11 <= idade <= 13:
+    print('Categopria: Juvenil A')
+elif 14 <= idade <= 17:
+    print('Categoria: Juvenil B')
+elif idade < 5:
+    print('Nasdador muito novo para classificar')
+else:
+    print('Sênior - Maior de 18 anos')
+
+"""
+28 Faça um programa que leia três números positivos e efetue o calcule de uma das 
+seguintes médias de acordo com um valor numérico digitado pelo usuário?
+a - Geométrica: raiz cubica (x * y * z)
+b - Ponderada: (x + (2 * y) + (3 * z)) / 6
+c - Harmônica: 1 / ((1 / x) + (1 / y) + (1 / z))
+d - Aritimética: (x + y +z) / 3
+"""
+x = float(input('Digite o valor de x: '))
+y = float(input('Digite o valor de y: '))
+z = float(input('Digite o valor de z: '))
+
+form = input('Selegione uma das médias a ser calculada: '
+              '\n a - Geométrica '
+              '\n b - Ponderada '
+              '\n c - Harmônica '
+              '\n d - Aritimética '
+             '\n media : ')
+
+if form == 'a':
+    mediag = (x * y * z) ** (1/3)
+    print(f'O resultaqdo da media geométrica foi: {mediag}')
+elif form == 'b':
+    mediap = (x + (2 * y) + (3 * z)) / 6
+    print(f'O resultado da media ponderada foi: {mediap}')
+elif form == 'c':
+    mediah = 1 / ((1 / x) + (1 / y) + (1 / z))
+    print(f'O resultado da media harmônica foi: {mediah}')
+elif form == 'd':
+    mediaa = (x + y +z) / 3
+    print(f'O resultado da media aritimética foi: {mediaa}')
+else:
+    print('Não pertence há medias para ser calculadas')
+
+"""
+29 Faça uma prova matemática para crianças que estão aprendendo a somar números
+interiores menores do que 100. Escolha números aleatórios entre 1 e 100, e mostre na 
+tela a pergunta: qual é a soma de a + b, onde a e b são os números aleatórios. Peça a 
+resposta. Faça cinco perguntas ao aluno, e mostre para ele as perguntas as perguntas e 
+as respostas corretas, além de quantas vezes o aluno acertou.
+"""
+from random import randint
+
+num1 = randint(1, 100)
+num2 = randint(1, 100)
+
+print(f'Qual é a soma de {num1} + {num2}?')
+result1 = int(input(""))
+
+
+num3 = randint(1, 100)
+num4 = randint(1, 100)
+
+print(f'Qual é a soma de {num3} + {num4}?')
+result2 = int(input(""))
+
+
+num5 = randint(1, 100)
+num6 = randint(1, 100)
+
+print(f'Qual é a soma de {num5} + {num6}?')
+result3 = int(input(""))
+
+
+num7 = randint(1, 100)
+num8 = randint(1, 100)
+
+print(f'Qual é a soma de {num7} + {num8}?')
+result4 = int(input(""))
+
+
+num9 = randint(1, 100)
+num10 = randint(1, 100)
+
+print(f'Qual é a soma de {num9} + {num10}?')
+result5 = int(input(""))
+
+acertos = 0
+print()
+if result1 == (num1 + num2):
+    acertos += 1
+
+if result2 == (num3 + num4):
+    acertos += 1
+
+if result3 == (num5 + num6):
+    acertos += 1
+
+if result4 == (num7 + num8):
+    acertos += 1
+
+if result5 == (num9 + num10):
+    acertos += 1
+
+print('Respostas:')
+print(f'{num1} + {num2} = {num1 + num2}')
+print(f'{num3} + {num4} = {num3 + num4}')
+print(f'{num5} + {num6} = {num5 + num6}')
+print(f'{num7} + {num8} = {num7 + num8}')
+print(f'{num9} + {num10} = {num9 + num10}')
+print()
+
+print(f'Quantidade de acertos: {acertos}')
+
+"""
+30 Faça um programa que receba três números e mostre-os em ordem crescente
+"""
+x = int(input('Valor de x: '))
+y = int(input('Valor de y: '))
+z = int(input('Valor de z: '))
+
+if x > y:
+    print(f'{x}, {y}, {z}')
