4013207c082e3fcffe2458a49877e80b451f572a
Carlos M Valenca <cavac10@gmail.com>
2022-09-20 06:33:07 -0300

    Exercicio ficha 2 exercicios 31 a 41

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio4.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio4.py
@@ -34,5 +34,38 @@ else:
     print('Algo deu errado')
 
 """
-32 
+32 Escreva um programa que leia o código do produto escolhido no cardapio de uma lanchonete
+e a quantidade. O programa deve calcular o valor sa ser pago por aquele lanche. Considere
+que a cada execução somente será calculado um pedido. O cardapio da lanchonete segue o padrão abaixo:
+Especificação      |  Código    |     Preço
+Cachorro Quente    |    100     |     1.20  
+Bauru Simples      |    101     |     1.30  
+Bauru com Ovo      |    102     |     1.50  
+Hamburguer         |    103     |     1.20   
+Cheeseburguer      |    104     |     1.70
+Suco               |    105     |     2.20   
+Refrigerante       |    106     |     1.00
+"""
+cod = int(input('Digite o codigo do produto: '))
+x = int(input('Digite a quantidade do produto'))
+
+if cod == 100:
+    print(f'Total a ser pago: {1.2 * x}')
+elif cod == 101:
+    print(f'Total a ser pago: {1.3 * x}')
+elif cod == 102:
+    print(f'Total a ser pago: {1.5 * x}')
+elif cod == 103:
+    print(f'Total a ser pago: {1.2 * x}')
+elif cod == 104:
+    print(f'Total a ser pago: {1.7 * x}')
+elif cod == 105:
+    print(f'Total a ser pago: {2.2 * x}')
+elif cod == 106:
+    print(f'Total a ser pago: {1.3 * x}')
+else:
+    print('Código inválido!')
+
+"""
+33 
 """
