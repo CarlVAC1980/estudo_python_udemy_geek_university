47ffed7181c007ef71823113790466da1e3d778b
Carlos M Valenca <cavac10@gmail.com>
2022-09-20 07:26:48 -0300

    Exercicio ficha 2 exercicios 31 a 41

--- 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio4.py
+++ 03estruturas_logicas_e_condicionais_em_python/exercicios/exercicio4.py
@@ -101,3 +101,45 @@ elif 120 < preco_novo <= 200:
     print('Caro')
 elif preco_novo > 200:
     print('Muito Caro')
+
+"""
+34 Leia a nota e o npúmero de faltas de um aluno, e escreva seu conceito. De acordo com a 
+tabela abaixo, quando o aluno tem mais de 20 faltas ocorre uma redução de conceito.
+
+|    Nota    |    Conceito(até 20 faltas)    |    Conceito(mais de 20 faltas)    |
+|9.0 até 10.0|               A               |                  B                |
+|7.5 até 8.9 |               B               |                  C                |
+|5.0 até 7.4 |               C               |                  D                |
+|4.0 até 4.9 |               D               |                  E                |
+|0.0 até 3.9 |               E               |                  E                |
+"""
+nota = float(input('Digite a nota: '))
+faltas = int(input('Digite a quantidade de faltas: '))
+
+if 9.0 >= nota <= 10.0:
+    if faltas <= 20:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito A')
+    else:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito B')
+elif 7.5 >= nota <= 8.9:
+    if faltas <= 20:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito B')
+    else:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito C')
+elif 5.0 >= nota <= 7.4:
+    if faltas <= 20:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito C')
+    else:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito D')
+elif 4.0 >= nota <= 4.9:
+    if faltas <= 20:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito D')
+    else:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito E')
+elif 0.0 >= nota <= 3.9:
+    if faltas <= 20:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito E')
+    else:
+        print(f'Nota: {nota} - Faltas: {faltas} - Conteito E')
+else:
+    print('Nota inválida')
