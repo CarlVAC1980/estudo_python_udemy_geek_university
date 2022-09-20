7385c7e97e557477a874fff56706ee4e02532c4e
Carlos M Valenca <cavac10@gmail.com>
2022-09-20 07:48:14 -0300

    Coelcoes em Python - Listas

---
+++ 05colecoes_em_python/listas.py
@@ -0,0 +1,15 @@
+"""
+Listas
+
+Listas em Python funcionam como vetores/matrizes (arrays) em outras linguagens, com a diferença
+de serem DINÂMICOS e também de podermos colocar QUALQUER tipo de dado
+
+Linguagens C/Java: Arrays
+    - Possuem tamanho e tipo de dado fixo:
+    Ou seja, nestas linguagens se você cirar um array do tipo int e tamanho 5, este array
+    será SEMPRE do tipo inteiro e poderá ter SEMPRE o maximo de 5 valores.
+
+Já em Python:
+
+- Dinâmico: Não possui tamanho fixo: Ou seja, podemos criar uma lista e simplesmente ir adicionando elementos;
+"""
