d9f934790e8074b38ca9f8b4dbb234dc638322f3
Carlos M Valenca <cavac10@gmail.com>
2022-09-21 08:48:11 -0300

    Listas - type

--- 05colecoes_em_python/listas.py
+++ 05colecoes_em_python/listas.py
@@ -13,4 +13,28 @@ Já em Python:
 
 - Dinâmico: Não possuem tamanho fixo: Ou seja, podemos criar uma lista e simplesmente ir adicionando elementos;
 - Qualquer tipo de dado: Não possuem tipo de dado fdixo: Ou seja, podemos colocar qualquer tipo de dado;
+
+As listas em Python são representadas por colchetes []
 """
+type([])
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Geek University')
+
+# Podemos facilmente checar se determinado valor está contido na lista
+num = 18
+if num in lista4:
+    print(f'Encontrei o número {num}')
+else:
+    print(f'Não encontrei o número {num}')
+
+# Podemos facilmente ordenar uma lista
+
+
